6a8d68418b7fe33916f483e21972641afc3b8010
SamFredrickson <svyatoslav19cm@mail.ru>
2021-06-02 03:01:52 +0300

    up

--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -4,8 +4,8 @@
 
 <div class="container">
     <div class="row">
-        <div class="col-md-8">
-            <div class="card mb-3">
+        <div class="col-md-8 order-md-1 order-sm-2">
+            <div class="card xs-3">
                 <div class="row no-gutters">
                   <div class="col-md-4">
                     <img src="..." class="card-img" alt="...">
@@ -20,9 +20,36 @@
                 </div>
               </div>
         </div>
-        <div class="col-md-4">
+        <div class="col-md-4 order-md-2 order-sm-1 mb-2">
+          <h4 class="text-center">Фильтрация:</h4>
             <form action="">
-                
+                <div class="form-row">
+                  <div class="form-group col-md-12">
+                    <label for="title">Название видео</label>
+                    <input type="text" class="form-control" id="title" placeholder="Название">
+                  </div>
+                </div>
+                <div class="form-row">
+                  <div class="form-group col-md-6">
+                    <label for="queue">Очередь</label>
+                    <input type="text" class="form-control" id="queue" placeholder="Очередь">
+                  </div>
+                  <div class="form-group col-md-6">
+                    <label for="paltform">Платформа</label>
+                    <select id="paltform" class="form-control">
+                      <option value="Youtube">
+                        Youtube
+                      </option>
+                      <option value="GoodGame">
+                        GoodGame
+                      </option>
+                      <option value="Другая">
+                        Другая
+                      </option>
+                    </select>
+                  </div>
+                </div>
+                <button type="submit" class="btn btn-primary float-right">Применить</button>
             </form>
         </div>
     </div>
